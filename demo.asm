400b5794039a3f898228b75418e891baee5bd3a9
llyan88 <lillianyan115@gmail.com>
2026-04-13 23:56:01 -0400

    Update ADC address constants in demo.asm

--- demo.asm
+++ demo.asm
@@ -5,6 +5,6 @@ ORG 0
 ;IO address constants
 Hex0:       EQU 004
 Hex1:       EQU 005
-ADCout:     EQU &H0C2
-ADCstatus:  EQU &H0C1
-ADCconfig:  EQU &H0C0
+ADCout:     EQU 194
+ADCstatus:  EQU 193
+ADCconfig:  EQU 192
