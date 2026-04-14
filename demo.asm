c2ec25402d4c5ba024b65dc5ced825b78457b4a5
llyan88 <lillianyan115@gmail.com>
2026-04-14 00:29:43 -0400

    ADC sampling + averaging loop in demo.asm

--- demo.asm
+++ demo.asm
@@ -1,6 +1,37 @@
 ORG 0
 
+Loop:
+    LOAD chan0
+    OUT ADCconfig
 
+ADCwait: 
+    IN ADCstatus
+    SUB chan2 ;checks for status 1 busy 0
+    JZERO ADCread
+    JUMP ADCwait
+
+ADCread:
+    IN ADCout
+    ADD sum
+    STORE sum
+    AND chan0 ; AC=0
+    ADDI 1
+    ADD count
+    STORE count
+    SUB X ; pick number for sample number, X=2^n
+    JZERO Finish
+    JUMP Loop
+
+Finish:
+    LOAD sum
+    SHIFT X ; X=n
+    OUT Hex0 
+
+;Vars
+chan0: DW &B0000000000
+chan2: DW &B0000000010
+count: DW &B0000000000
+sum:   DW &B0000000000
 
 ;IO address constants
 Hex0:       EQU 004
