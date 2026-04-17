84053fc42ae7d5aac38af68c100dbc9b3dcd5bcb
Neha-Donekal <121188240+Neha-Donekal@users.noreply.github.com>
2026-04-17 16:56:05 -0400

    Update demo.asm

--- demo.asm
+++ demo.asm
@@ -1,52 +1,52 @@
 ORG 0
 
 Reset:
-	IN Switches
-	JNZ Reset
+    IN    Switches
+    JNZ   Reset       
 
-Rand: 
-	IN Switches
-	AND Bit9
-	JNZ Next
-	LOAD randNum
-	ADDI 1
-	STORE randNum
-	JUMP Rand
+Rand:
+    IN    Switches
+    AND   Bit9
+    JNZ   Next        
+    LOAD  randNum
+    ADDI  1
+    STORE randNum
+    JUMP  Rand
 
-Next: 
-	LOAD randNum
-	AND down12 ;mod 12 bits
-	OUT Hex0
-
+Next:
+    
+    LOAD  Zero
+    STORE sum
+    STORE count
+    
+    
 Loop:
-    LOAD chan0
-    OUT ADCconfig
+    LOAD  chan0
+    OUT   ADCconfig
 
-ADCwait: 
-    IN ADCstatus
-    SUB chan2 ;checks for status 1 busy 0
+ADCwait:
+    IN    ADCstatus
+    SUB   chan2       ; Check if busy (bit 1)
     JZERO ADCread
-    JUMP ADCwait
+    JUMP  ADCwait
 
 ADCread:
-    IN ADCout
-	OUT Hex0
-    ADD sum
+    IN    ADCout
+    OUT   Hex0        
+    ADD   sum
     STORE sum
-    AND chan0 ; AC=0
-    ADDI 1
-    ADD count
+    LOAD  count
+    ADDI  1
     STORE count
-    LOAD count
-    ADDI -256 ; pick number for sample number, X=2^n
+    ADDI   -256        
     JZERO Finish
-    JUMP Loop
+    JUMP  Loop
 
 Finish:
-    LOAD sum
-    SHIFT -8 ; X=n
-	Store target
-    OUT Hex0
+    LOAD  sum
+    SHIFT -8          
+    STORE target      
+    OUT   Hex0
 
 WaitGuess:
     IN    Switches
@@ -60,43 +60,53 @@ ReadPot:
 PotWait:              
     IN    ADCstatus
     SUB   chan2
-    JZERO PotReadHey
+    JZERO PotRead
     JUMP  PotWait
 
 PotRead:
     IN    ADCout      
     SUB   target      
-    JPOS  AbsValue 
-	;Neg
+    JPOS  AbsValue    
 
 AbsValue:
-	;...
+    SUB   Threshold   
+    JNEG  Correct
+    JUMP  WaitGuess   
 
 Correct:
-	;...
-
+    LOAD  score
+    ADDI  1
+    STORE score
+    OUT   Hex1        
+    
+    
 ReleaseWait:
     IN    Switches
-    AND   Bit0        ; Wait for Switch 0 to be flipped DOWN
+    AND   Bit0
     JNZ   ReleaseWait 
-    JUMP  Next
-
+    JUMP  Next        
+    
 ;Vars
 target:    DW 0
 score:     DW 0
-chan0: DW &B0000000000
-chan2: DW &B0000000010
-count: DW &B0000000000
-sum:   DW &B0000000000
-randNum: DW 0
-Bit0: DW &B000000000001
-Bit9: DW &B01000000000
-down12: DW &B0111111111111
+count:     DW 0
+sum:       DW 0
+randNum:   DW 0
+Threshold: DW 10      
+Zero:      DW 0
 
-;IO address constants
-Switches:   EQU 000
-Hex0:       EQU 004
-Hex1:       EQU 005
-ADCout:     EQU 194
-ADCstatus:  EQU 193
-ADCconfig:  EQU 192
+
+; Bit Masks
+Bit0:      DW &B000000000001
+Bit9:      DW &B010000000000
+chan0:     DW &B000000000000
+chan2:     DW &B000000000010
+12Down:    DW &B011111111111
+
+; I/O Address Constants
+Switches:  EQU 000
+Hex0:      EQU 004
+Hex1:      EQU 005
+ADCout:    EQU 194
+ADCstatus: EQU 193
+ADCconfig: EQU 192
